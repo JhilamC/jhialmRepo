dc3b1e4914fb7201fe56cce300495e4d7077b274
jchatterje <jchatterje@ZHOBLI34168.hob.wiley.com>
2016-09-06 09:48:01 -0400

    first commit

---
+++ helloworld-java/src/main/java/com/wiley/hellworld/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.wiley.hellworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld {
+	public static void main( String[] args ){
+		HelloWorld helloWorld = new HelloWorld("Hello World!");
+		helloWorld.printMessage();
+	}
+
+	private String message;
+
+	public HelloWorld(String message){
+		this.message = message;
+	}
+
+
+	public String printMessage(){
+		System.out.println(message);
+		return message;
+	}  
+}
---
+++ helloworld-java/src/test/java/com/wiley/hellworld/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.wiley.hellworld;
+
+import junit.framework.TestCase;
+import com.wiley.hellworld.HelloWorld;
+
+public class HelloWorldTest  extends TestCase {
+
+	public HelloWorldTest(String name) {
+		super( name );
+	}
+
+	public void testHelloWorld() throws Exception {
+		String testMessage="Hello World!";
+		HelloWorld helloWorld = new HelloWorld("Hello World!");
+		assertEquals(testMessage,helloWorld.printMessage());
+	}
+}
